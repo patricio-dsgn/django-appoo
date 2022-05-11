d492921e834524fee78f3d61ddc712336d00f730
patricio-dsgn <patricio.ngg@gmail.com>
2022-05-10 20:39:59 -0400

    100%

--- appoo/settings.py
+++ appoo/settings.py
@@ -110,8 +110,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
+LANGUAGE_CODE = 'es-es'
+# LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
